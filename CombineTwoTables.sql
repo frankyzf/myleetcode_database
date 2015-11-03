e4ad8683c1ed1c7e27c0536fd4747af884694ab7
frankyzf <frankyzf@gmail.com>
2015-11-03 22:23:08 +0800

    left join ON

---
+++ CombineTwoTables.sql
@@ -0,0 +1,2 @@
+/*Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:*/
+select p.FirstName, p.LastName, a.City, a.State from Person p left join Address a on p.PersonId = a.PersonId 
